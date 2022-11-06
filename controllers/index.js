531780b9878950a1b4092783a92490f313e7e5bc
Your Name <you@example.com>
2022-11-06 18:38:35 -0500

    added food model and upated server.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const express = require('express');      //get the express package
 const drinks = require('../models/drinks'); //<--get our drinks array
-// const food = require('../models/food');     //<==get our foods array
+const food = require('../models/food');     //<==get our foods array
 const router = express.Router(); //<-- we CANOT do const app = express() because 
 //we did that in server.js. Therefore we must do const router = express.Router()
 //and replace all the app.get to router.get
@@ -11,18 +11,13 @@ router.get('/', (request,response) => {
 })
 
 router.get('/drinks', (request,response) => {
-   response.render('drinks_index.ejs', { drinks })
+   response.render('drinks_index.ejs', { drinks, food })
 })
 
 router.get('/drinks/:id', (request,response) => {
     let oneDrink = drinks [request.params.id]
     response.render('drinks_show.ejs', { oneDrink } )   
 })
-
-router.get("/fruits/:indexOfFruitsArray", (req, res) => {
-
-    res.send(fruits[req.params.indexOfFruitsArray]);
- });
 
 module.exports = router; //<---- making this router exportable
 
