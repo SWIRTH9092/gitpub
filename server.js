1aa819835f45828202550180c132f97085228193
Your Name <you@example.com>
2022-11-06 16:36:47 -0500

    added css file and styling for drink list

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.POR || 3000;
 
+app.use(express.static(__dirname + '/public'))
+// app.use(express.static(__dirname + '/public')); // this is how we add css to 
+// our templates in html.
+
 const drinkFoodController = require('./controllers/index');
 
 app.use(drinkFoodController);
